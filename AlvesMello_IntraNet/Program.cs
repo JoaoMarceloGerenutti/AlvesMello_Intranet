6151ca6034510d6a4e5512f7ba15fa5c8a87d909
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-07-26 13:16:30 -0300

    Unifying the Startup and Program classes

--- AlvesMello_IntraNet/Program.cs
+++ AlvesMello_IntraNet/Program.cs
@@ -1,19 +1,115 @@
-namespace AlvesMello_IntraNet;
+using AlvesMello_IntraNet.Context;
+using AlvesMello_IntraNet.Models;
+using AlvesMello_IntraNet.Repositories;
+using AlvesMello_IntraNet.Repositories.Interfaces;
+using AlvesMello_IntraNet.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Home/AccessDenied");
+builder.Services.Configure<ConfigurationImages>(builder.Configuration.GetSection("ConfigurationImagesFolder"));
+
+builder.Services.Configure<IdentityOptions>(options =>
 {
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 1;
+});
 
-    public static void Main(string[] args)
+builder.Services.AddTransient<ISiteRepository, SiteRepository>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin",
+        policy =>
+        {
+            policy.RequireRole("Admin");
+        });
+});
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => FavoriteSite.GetFavorite(sp));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+CreateUsersProfiles(app);
+
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapControllerRoute(
+        name: "filterCategory",
+        pattern: "Site/{action}/{category?}",
+        defaults: new { Controller = "Site", Action = "List" }
+    );
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+//Configure the HTTP request pipeline.
+app.Run();
+
+void CreateUsersProfiles(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
     {
-        CreateHostBuilder(args)
-            .Build()
-            .Run();
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedRoles();
+        service.SeedUsers();
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
 }
