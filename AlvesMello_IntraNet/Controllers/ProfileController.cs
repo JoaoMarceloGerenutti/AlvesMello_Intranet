b0865c5ff81eab2cdb066ed7c1bfaa21ea251662
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-07-29 13:51:43 -0300

    Created profile page frontend

---
+++ AlvesMello_IntraNet/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlvesMello_IntraNet.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
