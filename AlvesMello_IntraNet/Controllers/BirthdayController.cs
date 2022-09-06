29f5734f1a5be7d9c736c9824202d47de74525bf
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-09-06 13:44:50 -0300

    Added birthdays page and header shortcut.

---
+++ AlvesMello_IntraNet/Controllers/BirthdayController.cs
@@ -0,0 +1,26 @@
+﻿using AlvesMello_IntraNet.Context;
+using AlvesMello_IntraNet.Models;
+using AlvesMello_IntraNet.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlvesMello_IntraNet.Controllers
+{
+	public class BirthdayController : Controller
+	{
+		private readonly AppDbContext _context;
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public BirthdayController(AppDbContext context, UserManager<ApplicationUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			var users = _userManager.Users.OrderBy(d => d.BirthDate.Month);
+			return View(users);
+		}
+	}
+}
